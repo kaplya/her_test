0ba84956faadc3c243686f7da4d6f36fc571ed98
Timur A. Kaplya <timur.kaplya@gmail.com>
2013-03-15 18:31:29 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerTest::Application.config.secret_token = '14b4e1302fd1ff8ef5aa9ae223604d5da35f03a4a8b99c6f060ca0ed0168f56372a4fcc691ede88208ccddf4ea3c1b215a413a3a3a1200f81d771c341c305247'
